f5f70131f6cf4e94ec03cb99b13d7938ea7ca9ad
waqaskanju <waqaskanju@gmail.com>
2026-03-15 12:29:25 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Index file</title>
+</head>
+<body>
+  THis is my first index file.
+</body>
+</html>
